b69bcb673117f43df4596c6d5b013f429dab7123
rnuravtsev <muravtsev21@gmail.com>
2021-10-05 19:45:49 +0300

    Update slider and photos

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -4,11 +4,23 @@ import SwiperCore, { Navigation, Pagination } from 'swiper/core'
 SwiperCore.use([Navigation, Pagination, Autoplay])
 
 const createDefaultSwiper = ({ container, pagination }) => new Swiper(container, {
-  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
   // autoplay: {
   //   delay: 3000,
   // },
-  // autoHeight: true,
   centeredSlidesBounds: true,
   speed: 750,
 
